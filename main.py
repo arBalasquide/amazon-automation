6f9fbdcdd4c9bed142a8ab6ee0b06c56ba18c21d
arBalasquide <adrian.balasquide@gmail.com>
2020-04-30 03:48:54 -0400

    USA Amazon fix

--- main.py
+++ main.py
@@ -1,69 +1,82 @@
-#coding: utf-8
-
 import time
+import os
 from datetime import datetime
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
 
-LOGIN_ID = '[メールアドレス]'
-LOGIN_PASSWORD = '[パスワード]'
+load_dotenv()
 
-ITEM_URL = 'https://www.amazon.co.jp/dp/??????????????????'    # 商品URL
+CHROMEDRIVER_PATH = '/bin/chromedriver'
+
+options = Options()
+options.add_argument("user-data-dir=./profile") # to bypass OTP verification
+
+LOGIN_PASSWORD = os.getenv("LOGIN_PASSWORD")
+
+ITEM_URL = 'https://www.amazon.com/dp/????????/' # Product URL
 
 ACCEPT_SHOP = 'Amazon'
-LIMIT_VALUE = 33500    # 最低金額
+LIMIT_VALUE = 320      # Max price
+
 
 def l(str):
     print("%s : %s"%(datetime.now().strftime("%Y/%m/%d %H:%M:%S"),str))
 
 if __name__ == '__main__':
+    PURCHASED = False
 
-    # ブラウザの起動
+    # Boot up the browser
     try:
-        b = webdriver.Chrome('./chromedriver')
+        b = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
         b.get(ITEM_URL)
     except:
         l('Failed to open browser.')
         exit()
 
-    while True:
-        # 在庫確認
+    while True and not PURCHASED:
         while True:
             try:
-                # 販売元確認
+                # Confirm seller
                 shop = b.find_element_by_id('merchant-info').text
-                shop = shop.split('が販売')[0].split('この商品は、')[1]
+                shop = shop.split('Ships from and sold by ')[1] # may not work for other shops
 
                 if ACCEPT_SHOP not in shop:
-                    raise Exception("not Amazon.")
+                    l("NOT IN AMAZON")
+                    time.sleep(60)
+                    b.refresh()
+                    continue
 
-                # カードに入れる
-                b.find_element_by_id('add-to-cart-button').click()
+                # Add to cart
+                b.find_element_by_id('add-to-cart-button-ubb').click()
                 break
             except:
+                l('EXCEPTION OCCURED.')
                 time.sleep(60)
                 b.refresh()
 
-        # 購入手続き
-        b.get('https://www.amazon.co.jp/gp/cart/view.html/ref=nav_cart')
-        b.find_element_by_name('proceedToCheckout').click()
+        # Go to cart and checkout
+        b.get('https://www.amazon.com/gp/cart/view.html/ref=nav_cart')
+        b.find_element_by_name('a-button-input').click()
 
-        # ログイン
+        # Purchase re-log in verification
         try:
-            b.find_element_by_id('ap_email').send_keys(LOGIN_ID)
             b.find_element_by_id('ap_password').send_keys(LOGIN_PASSWORD)
             b.find_element_by_id('signInSubmit').click()
         except:
             l('LOGIN PASS.')
             pass
 
-        # 値段の確認
+        # Confirm price is not too high
         p = b.find_element_by_css_selector('td.grand-total-price').text
         if int(p.split(' ')[1].replace(',', '')) > LIMIT_VALUE:
-            l('PLICE IS TOO LARGE.')
+            print(int(p.split(' ')[1].replace(',', '')))
+            l('PRICE IS TOO LARGE.')
             continue
 
-        # 注文の確定
-        b.find_element_by_name('placeYourOrder1').click()
+        # Accept the order
+        else:
+            b.find_element_by_name('placeYourOrder1').click()
+            PURCHASED = True
         break
-
-    l('ALL DONE.')
+    l('ITEM PURCHASED')
